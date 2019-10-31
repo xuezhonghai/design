da420db8e97ec8499c87503794a1391869169f77
薛中海 <zhonghai.xue@pactera.com>
2019-10-31 09:32:34 +0800

    feature: 新增备忘录模式

---
+++ src/test/java/zhonghai/xue/design/memento/MementoTest.java
@@ -0,0 +1,26 @@
+package zhonghai.xue.design.memento;
+
+import org.junit.jupiter.api.Test;
+import zhonghai.xue.design.behavior.memento.CareTaker;
+import zhonghai.xue.design.behavior.memento.Originator;
+
+public class MementoTest {
+
+    @Test
+    public void test1(){
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+        originator.setState("State #1");
+        originator.setState("State #2");
+        careTaker.add(originator.saveStateToMemento());
+        originator.setState("State #3");
+        careTaker.add(originator.saveStateToMemento());
+        originator.setState("State #4");
+
+        System.out.println("Current State: " + originator.getState());
+        originator.getStateFromMemento(careTaker.get(0));
+        System.out.println("First saved State: " + originator.getState());
+        originator.getStateFromMemento(careTaker.get(1));
+        System.out.println("Second saved State: " + originator.getState());
+    }
+}
